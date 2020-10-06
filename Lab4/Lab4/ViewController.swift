de7bcc1e8b6cc6a26a093545490b91a7670f7652
Poketrumpeter <gregorio@fefland.com>
2020-10-05 20:25:40 -0600

    final push for Lab 4 and created project 1 outline project

--- Lab4/Lab4/ViewController.swift
+++ Lab4/Lab4/ViewController.swift
@@ -119,7 +119,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue{
             
             if self.view.frame.origin.y == 0{
-                self.view.frame.origin.y -= keyboardSize.height
+                self.view.frame.origin.y -= keyboardSize.height/1.5
             }
         }
     }
